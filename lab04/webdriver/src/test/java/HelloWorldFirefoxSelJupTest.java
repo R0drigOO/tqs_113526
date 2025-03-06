e860238f22d80aec216cd61aa8026c726cf3c703
R0drigOO <marquesrodrigo@ua.pt>
2025-03-06 10:44:18 +0000

    criaçao maven project ex2

--- lab04/webdriver/src/test/java/HelloWorldFirefoxSelJupTest.java
+++ lab04/webdriver/src/test/java/HelloWorldFirefoxSelJupTest.java
@@ -1,11 +1,16 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Duration;
 
 import io.github.bonigarcia.seljup.SeleniumJupiter;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -39,6 +44,29 @@ class HelloWorldFirefoxSelJupTest {
 
         // Verify
         assertThat(title).isEqualTo("Hands-On Selenium WebDriver with Java");
+    }
+
+    @Test
+    @Disabled
+    void simple_test(){
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        driver.getTitle();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        message.getText();
+
+        driver.quit();
     }
 
     @AfterEach
